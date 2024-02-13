20961152fb7ec461dab9672cef5eac5de7f97f1b
Hadil Sghair <hadil03sghair@gmail.com>
2024-02-13 17:30:23 +0100

    Add the spring project

---
+++ BackEnd/CareerLink/src/main/java/tn/esprit/careerlink/CareerLinkApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.careerlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerLinkApplication.class, args);
+    }
+
+}
